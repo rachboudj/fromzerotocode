ce6c168318f84496cba43367edd7825a04378e41
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-07-28 18:32:33 +0200

    ajout route vers crud cours

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,70 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+
+// Composant
+import Menu from '../components/Menu';
+
+// Page
+import Home from './pages/Home'
+
+// Admin 
+import CreateCategory from './pages/Admin/Category/CreateCategory'
+import Category from './pages/Admin/Category/Category'
+import EditCategory from './pages/Admin/Category/EditCategory';
+import Role from './pages/Admin/Role/Role';
+import CreateRole from './pages/Admin/Role/CreateRole';
+import EditRole from './pages/Admin/Role/EditRole';
+import Course from './pages/Admin/Course/Course';
+import CreateCourses from './pages/Admin/Course/CreateCourse';
+import EditCourses from './pages/Admin/Course/EditCourse';
+import Tutorial from './pages/Admin/Tutorial/Tutorial';
+import CreateTutorial from './pages/Admin/Tutorial/CreateTutorial';
+import EditTutorial from './pages/Admin/Tutorial/EditTutorial';
 
 function App() {
 
   return (
     <>
-     <h1>test</h1>
+      <Router>
+        <ToastContainer 
+        oastClassName={(context) =>
+          contextClass[context?.type || "default"] +
+          "relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
+        }
+        bodyClassName={() => "poppins-medium flex text-gray-700 text-sm font-white font-med block p-3"}
+        />
+        <Menu />
+        <div className='content overflow-hidden w-full h-screen overflow-y-scroll'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* User */}
+            {/* <Route path="/user-admin" element={<UserAdmin />} />
+            <Route path="/admin/users/:id/edit" element={<EditUserPage />} /> */}
+            {/* Cours Admin */}
+            <Route path="/admin/courses" element={<Course />} />
+            <Route path="/admin/courses/new" element={<CreateCourses />} />
+            <Route path="/admin/courses/:id/edit" element={<EditCourses />} />
+            {/* Tutorial Admin */}
+            <Route path="/admin/tutorial" element={<Tutorial />} />
+            <Route path="/admin/tutorial/new" element={<CreateTutorial />} />
+            <Route path="/admin/tutorial/:id/edit" element={<EditTutorial />} />
+            {/* Catégorie */}
+            <Route path="/admin/category" element={<Category />} />
+            <Route path="/admin/category/new" element={<CreateCategory />} />
+            <Route path="/admin/category/:id/edit" element={<EditCategory />} />
+            {/* Rôles */}
+            <Route path="/admin/roles" element={<Role />} />
+            <Route path="/admin/roles/new" element={<CreateRole />} />
+            <Route path="/admin/roles/:id/edit" element={<EditRole />} />
+            {/* Login et connexion */}
+            {/* <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} /> */}
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
