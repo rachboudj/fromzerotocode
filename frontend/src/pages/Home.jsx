8bd4c18016bc0588dacd64da3c0ff3f56056de10
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-07-29 18:21:41 +0200

    affichage des cours sur l'accueil

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 export default function Home() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/courses`);
+        setCourses(response.data);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
-    </div>
+          {courses.length > 0 ? (
+            <ul>
+              {courses.map((course) => (
+                <div key={course.id_course}>
+                  <span>{course.category.title}</span>
+                  <h2>{course.title}</h2>
+                  <h2>{course.description}</h2>
+                  <Link className='font-medium text-cyan-500 dark:text-blue-500 hover:underline mr-2' to={`/detail-course/${course.id_course}`}>Voir le tuto</Link>
+                </div>
+              ))}
+            </ul>
+          ) : (
+            <p>Pas de cours de disponible.</p>
+          )}
+        </div>
   )
 }
