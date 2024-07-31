52ad0b30f30c2e013382e72a699398ff40dcdd1a
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-07-31 11:29:10 +0200

    ajout page + un peu de contenu

---
+++ frontend/src/pages/Apropos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Apropos() {
+  return (
+    <div>
+      <h1>À propos</h1>
+
+      <p>Le site web FromZeroToCode a pour objectif principal de devenir une ressource incontournable pour les développeurs web débutants et intermédiaires, ainsi que pour les étudiants en développement web. Nous visons à fournir des tutoriels complets et de haute qualité sur diverses technologies de développement web, accompagnés de projets complets pratiques et pertinents.</p>
+      <p> Ce projet s'inscrit dans le cadre de ma spécialité en développement web à l'école. Le site proposera des tutoriels liés à cette spécialité, avec un développement au niveau marketing, réseaux sociaux et graphiques.</p>
+    </div>
+  )
+}
