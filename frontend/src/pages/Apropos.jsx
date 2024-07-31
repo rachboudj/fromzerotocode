e5306ed4e199ed15331d8adc24152edce8a6b2e7
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-07-31 20:50:52 +0200

    ajout style

--- frontend/src/pages/Apropos.jsx
+++ frontend/src/pages/Apropos.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function Apropos() {
   return (
-    <div>
-      <h1>À propos</h1>
+    <div className='p-10'>
+      <div className='mt-10 p-10 border-2 border-black bg-white'>
+        <h1 className='font-heading font-bold text-3xl'>À propos</h1>
 
-      <p>Le site web FromZeroToCode a pour objectif principal de devenir une ressource incontournable pour les développeurs web débutants et intermédiaires, ainsi que pour les étudiants en développement web. Nous visons à fournir des tutoriels complets et de haute qualité sur diverses technologies de développement web, accompagnés de projets complets pratiques et pertinents.</p>
-      <p> Ce projet s'inscrit dans le cadre de ma spécialité en développement web à l'école. Le site proposera des tutoriels liés à cette spécialité, avec un développement au niveau marketing, réseaux sociaux et graphiques.</p>
+        <p className='mt-6'>Le site web FromZeroToCode a pour objectif principal de devenir une ressource incontournable pour les développeurs web débutants et intermédiaires, ainsi que pour les étudiants en développement web. Nous visons à fournir des tutoriels complets et de haute qualité sur diverses technologies de développement web, accompagnés de projets complets pratiques et pertinents.</p>
+        <p>Ce projet s'inscrit dans le cadre de ma spécialité en développement web à l'école. Le site proposera des tutoriels liés à cette spécialité, avec un développement au niveau marketing, réseaux sociaux et graphiques.</p>
+      </div>
     </div>
   )
 }
