a278242ac7301b801ea02afbac535c9a2791e0d0
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-07-31 11:33:36 +0200

    accès menu Admin et SuperAdmin + utilisateur connecté ou non

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -12,16 +12,21 @@ export default function Menu() {
     const submenuRef = useRef(null);
     const [submenuVisible, setSubmenuVisible] = useState(false);
     const [userRole, setUserRole] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
         const accessToken = localStorage.getItem("accessToken");
         if (accessToken) {
             const decodedToken = jwtDecode(accessToken);
             setUserRole(decodedToken.roles);
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
         }
     }, []);
 
     const hasAccess = userRole.includes('Admin');
+    const hasSuperAdminAccess = userRole.includes('SuperAdmin');
 
     const handleMouseEnter = (event) => {
         setSubmenuVisible(!submenuVisible);
@@ -30,6 +35,7 @@ export default function Menu() {
     const handleLogout = () => {
         localStorage.removeItem("accessToken");
         navigate('/login');
+        window.location.reload();
     };
 
     return (
@@ -40,28 +46,49 @@ export default function Menu() {
                     <span className='font-medium ml-3'>From Zero To Code</span>
                 </div>
 
-                <div className="flex space-x-4 items-center">
-                    <div className='relative'>
-                        <button onClick={handleMouseEnter}><img className="w-8 h-8 rounded-full shadow-lg" src={logo} alt="Bonnie image" /></button>
-                        <div
-                            ref={submenuRef}
-                            className={`${submenuVisible ? '' : 'hidden'} absolute submenu right-1 z-10 mt-2 w-48 origin-top-right rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5`}>
-                            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                                {hasAccess && (
-                                    <>
+                <div className='flex items-center'>
+                    <ul className='flex items-center '>
+                        <li><a href="/" className="" role="menuitem">Accueil</a></li>
+                        <li><a href="/a-propos" className="" role="menuitem">À propos</a></li>
+                        <li><a href="/les-tutos" className="" role="menuitem">Les tutos</a></li>
+                    </ul>
+                </div>
 
-                                        <a href="/admin/user" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Utilisateurs</a>
-                                        <a href="/admin/courses" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Cours</a>
-                                        <a href="/admin/tutorial" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Tutoriels</a>
-                                        <a href="/admin/category" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Catégorie</a>
-                                        <a href="/admin/roles" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Rôles</a>
-                                        <hr />
+                <div className="flex space-x-4 items-center">
+                    {isLoggedIn ? (
+                        <div className='relative'>
+                            <button onClick={handleMouseEnter}><img className="w-8 h-8 rounded-full shadow-lg" src={logo} alt="Bonnie image" /></button>
+                            <div
+                                ref={submenuRef}
+                                className={`${submenuVisible ? '' : 'hidden'} absolute submenu right-1 z-10 mt-2 w-48 origin-top-right rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5`}>
+                                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                    <a href="/profil" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Profil</a>
+
+                                    {hasAccess && (
+                                        <>
+                                            <span className="block mt-4 uppercase px-4 py-2 font-medium text-xs text-gray-700 hover:bg-gray-100 hover:text-gray-900">Administrateur</span>
+                                            {hasSuperAdminAccess && (
+                                                <>
+                                            <a href="/admin/user" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Utilisateurs</a>
+                                            <a href="/admin/roles" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Rôles</a>
+                                            </>
+                                            )}
+                                            <a href="/admin/category" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Catégorie</a>
+                                            <a href="/admin/courses" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Cours</a>
+                                            <a href="/admin/tutorial" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Tutoriels</a>
                                         </>
-                                )}
-                                        <button className="block px-4 py-2 text-sm text-red-500  hover:bg-gray-100 hover:text-red-700" role="menuitem" onClick={handleLogout}>Se déconnecter</button>
+                                    )}
+                                    <hr />
+                                    <button className="block px-4 py-2 text-sm text-red-500  hover:bg-gray-100 hover:text-red-700" role="menuitem" onClick={handleLogout}>Se déconnecter</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div>
+                            <button className="px-4 py-2 text-sm text-blue-500 hover:text-blue-700" onClick={() => navigate('/login')}>Se connecter</button>
+                            <button className="px-4 py-2 text-sm text-blue-500 hover:text-blue-700" onClick={() => navigate('/register')}>S'inscrire</button>
+                        </div>
+                    )}
                 </div>
             </div>
         </nav>
