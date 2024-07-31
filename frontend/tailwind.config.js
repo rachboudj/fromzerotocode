77bc38b3af6ff9ae393d61656d2f45a5c3dc5415
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-07-31 20:28:34 +0200

    ajout font et media queries perso

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space Mono', 'system-ui', 'monospace'],
+        heading: ['Syne', 'serif'],
+      },
+      screens: {
+        'phone': {'max' : '375px'},
+        'tablet': {'max' : '768px'},
+        'laptop': {'max' : '1024px'},
+        'desktop': {'max' : '1280px'},
+      },
+    },
   },
   plugins: [],
 }
