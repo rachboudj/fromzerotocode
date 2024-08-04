f802b741c107c9541d9f7228cfb180594e610fac
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-08-04 11:48:28 +0200

    validation lien youtube

--- frontend/src/pages/Admin/Tutorial/CreateTutorial.jsx
+++ frontend/src/pages/Admin/Tutorial/CreateTutorial.jsx
@@ -27,8 +27,19 @@ export default function CreateTutorial() {
         setSelectedCourse(event.target.value);
     };
 
+    const isValidYouTubeURL = (url) => {
+        const regex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
+        return regex.test(url);
+    };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (!isValidYouTubeURL(content)) {
+            toast.error('Veuillez entrer une URL YouTube valide.');
+            return;
+        }
+
         const courseId = parseInt(selectedCourse);
 
         const tutorialData = {
@@ -71,7 +82,7 @@ export default function CreateTutorial() {
                         <input className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer' type="text" id="title" value={title} onChange={(event) => setTitle(event.target.value)} />
                     </div>
                     <div className='relative z-0 w-full mt-10 group'>
-                        <label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="content">Contenu</label>
+                        <label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="content">URL Youtube</label>
                         <input className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer' type="text" id="content" value={content} onChange={(event) => setContent(event.target.value)} />
                     </div>
                     <div className='relative z-0 w-full mt-10 group'>
--- frontend/src/pages/Admin/Tutorial/EditTutorial.jsx
+++ frontend/src/pages/Admin/Tutorial/EditTutorial.jsx
@@ -41,8 +41,18 @@ export default function EditTutorial() {
         setSelectedCourse(event.target.value);
     };
 
+    const isValidYouTubeURL = (url) => {
+        const regex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
+        return regex.test(url);
+    };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (!isValidYouTubeURL(content)) {
+            toast.error('Veuillez entrer une URL YouTube valide.');
+            return;
+        }
         const courseId = parseInt(selectedCourse);
 
         const tutorialData = {
@@ -80,15 +90,15 @@ export default function EditTutorial() {
 
                     <h1 className='text-3xl font-black'>Modifier le tutoriel</h1>
                     <div className='relative z-0 w-full mt-10 group'>
-                        <label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="title">Titre du tutoriel</label>
+                        <label className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="title">Titre du tutoriel</label>
                         <input className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer' type="text" id="title" value={title} onChange={(event) => setTitle(event.target.value)} />
                     </div>
                     <div className='relative z-0 w-full mt-10 group'>
-                        <label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="content">Contenu</label>
+                        <label className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="content">Contenu</label>
                         <input className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer' type="text" id="content" value={content} onChange={(event) => setContent(event.target.value)} />
                     </div>
                     <div className='relative z-0 w-full mt-10 group'>
-                        <label className='peer-focus:font-medium text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="course">Cours</label>
+                        <label className='peer-focus:font-medium text-xs text-gray-500 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6' htmlFor="course">Cours</label>
                         <select className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="course" value={selectedCourse} onChange={handleCourseChange}>
                             <option value="">-- Sélectionner un cours --</option>
                             {courses.map(course => (
@@ -98,7 +108,7 @@ export default function EditTutorial() {
                             ))}
                         </select>
                     </div>
-                    <button className='mt-5 text-white bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={handleSubmit}>Modifier le tutoriel</button>
+                    <button className='mt-5 text-white bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center' onClick={handleSubmit}>Modifier le tutoriel</button>
                 </div>
             </AdminRoute>
         </PrivateRoute>
